c414c3f4f4e2d19c021808985b280c4096931d0b
Jetix24 <82837974+Jetix24@users.noreply.github.com>
2024-02-15 16:59:02 -0600

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Hola Mundo. <br> Prattle está en construcción.</h1>
+      <h1>Hola Mundo. Prattle está en construcción.</h1>
     </main>
   );
 }
