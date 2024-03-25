86fbcd694f9d6c2ce6172ee1a5aedcc9a0a658ad
Jetix24 <josepabloaguirre02@gmail.com>
2024-03-25 15:23:46 -0600

    UPDATE: Iniciar sesion completo

--- src/app/logIn/page.js
+++ src/app/logIn/page.js
@@ -1,4 +1,5 @@
 import styles from "./logIn.module.css"
+import Link from 'next/link';
 import { FormLogIn} from '@/components/inicioSesion/FormLogIn/FormLogIn';
 import { ButtonGoogle } from '@/components/inicioSesion/ButtonGoogle/ButtonGoogle';
 export default function logIn() {
@@ -17,7 +18,21 @@ export default function logIn() {
                         <h2>Iniciar Sesión</h2>
                     </div>
                         <FormLogIn />
+                        <div className={styles.dividerContainer}>
+                            <div className={styles.line}></div>
+                            <span className={styles.orText}>o</span>
+                            <div className={styles.line}></div>
+                        </div>
                         <ButtonGoogle />
+                        <div className={styles.loginPrompt}>
+                            ¿Aún no tienes cuenta? {" "}
+                            <Link href="/signUp">
+                                Registrate
+                            </Link>
+                        </div>
+            <div className={styles.footer}>
+              Al continuar, aceptas las condiciones del servicio
+            </div>
                 </div>
             </div>
         </div>
