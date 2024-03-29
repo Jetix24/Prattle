6499eeed20a45d8f2a37cff328c076b3945b4ad0
Jetix24 <josepabloaguirre02@gmail.com>
2024-03-29 02:11:53 -0600

    ADD: carpeta side

---
+++ src/components/descripcion/FormPerfil/FormPerfil.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import styles from "./FormPerfil.module.css"
+
+export const FormPerfil = () => {
+    return (
+            <form className={styles.logInForm}>
+                <div className={styles.formGroup}>
+                    <label htmlFor="email">Correo</label>
+                    <div className={styles.inputIconContainer}>
+                        <input type="email" id="email" name="email" required placeholder="Ingresa tu correo"/>
+                        <img src="/icon/correo.png" alt="icon" className={styles.inputIcon} />
+                    </div>
+                </div>
+                <div className={styles.formGroup}>
+                    <label htmlFor="password">Contraseña</label>
+                    <div className={styles.inputIconContainer}>
+                            <input type="password" id="password" name="password" required placeholder="Ingresa tu contraseña"/>
+                            <img src="/icon/bloquear.png" alt="icon" className={styles.inputIcon} />
+                        </div>
+                </div>
+                    <button type="submit">Ingresar</button>
+            </form>
+    )
+}
