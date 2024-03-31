3ad57a22ac45fddb957080375360825962a91749
solofervqz <solofervazquez@gmail.com>
2024-03-31 02:25:53 -0600

    UseSession redirect to dashboard

--- src/components/inicioSesion/FormLogIn/FormLogIn.tsx
+++ src/components/inicioSesion/FormLogIn/FormLogIn.tsx
@@ -1,7 +1,20 @@
 import Link from "next/link"
 import styles from "./FormLogIn.module.css"
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export const FormLogIn = () => {
+
+    const { data: session } = useSession();
+    const router = useRouter();
+
+    useEffect(() => {
+        if (session) {
+          router.push('/dashboard');
+        }
+      }, [session, router]);
+
     return (
             <form className={styles.logInForm}>
                 <div className={styles.formGroup}>
