9694580bf3268324a60621f4ad4151f000b5b7ee
Jetix24 <josepabloaguirre02@gmail.com>
2024-05-13 13:47:12 -0600

    Update: Landing Page 3.3 Responsive Us

--- src/components/home/Us/Us.tsx
+++ src/components/home/Us/Us.tsx
@@ -34,7 +34,7 @@ export const Us = () => {
             <div className={styles.line3} style={{ backgroundColor: '#FF2E6B', width: '70%'}}></div>
             <div className={styles.line3} style={{ backgroundColor: '#00DAA1', width: '60%'}}></div>
             <div className={styles.line3} style={{ backgroundColor: '#FFC535', width: '50%'}}></div>
-            <div className={styles.line3} style={{ backgroundColor: '#009AAB', width: '40%'}}></div>
+            <div className={styles.line3} style={{ backgroundColor: '#009AAB', width: '40%', marginBottom:'10%'}}></div>
         </section>
     )
 };
