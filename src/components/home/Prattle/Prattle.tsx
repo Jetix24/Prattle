da22a6920792e0b7ee8ab26b97cb2761051a1849
Jetix24 <josepabloaguirre02@gmail.com>
2024-05-13 19:04:31 -0600

    Update: Landing Page 3.5 Responsive details

--- src/components/home/Prattle/Prattle.tsx
+++ src/components/home/Prattle/Prattle.tsx
@@ -14,7 +14,7 @@ return (
                 <p>Sumérgete en el fascinante mundo de Prattle y descubre cómo nuestra aplicación está transformando la forma en que te comunicas. Conoce cómo Prattle personaliza tus conversaciones, ofreciéndote recomendaciones contextuales y facilitando interacciones.</p>
             </div>
         </div>
-        <div className={styles.line} style={{ backgroundColor: '#FF2E6B', width: '70%', marginTop: '4%'}}></div>
+        <div className={styles.line} style={{ backgroundColor: '#FF2E6B', width: '70%'}}></div>
         <div className={styles.line} style={{ backgroundColor: '#00DAA1', width: '60%'}}></div>
         <div className={styles.line} style={{ backgroundColor: '#FFC535', width: '50%'}}></div>
         <div className={styles.line} style={{ backgroundColor: '#009AAB', width: '40%'}}></div>
