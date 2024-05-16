c552191770bd115757d6242f95185fdc8321da80
Jetix24 <josepabloaguirre02@gmail.com>
2024-05-15 19:21:56 -0600

    Update: Pruebas dashboard

--- src/components/user/descripcion/FormPerfil/FormPerfil.tsx
+++ src/components/user/descripcion/FormPerfil/FormPerfil.tsx
@@ -1,51 +1,24 @@
 "use client";
-import { User } from "@prisma/client";
-import styles from "./FormPerfil.module.css"
-import { useForm } from 'react-hook-form';
-import { useState, useEffect } from 'react';
-import { useRouter } from 'next/router';
+import useRoutes from "@/app/hooks/useRoutes";
+import { useState } from "react";
+import styles from "./FormPerfil.module.css";
+import {User} from "@prisma/client";
 
 interface FormPerfilProps {
-  currentUser: User;
+    currentUser: User
 }
 
-export const FormPerfil = ({ currentUser }: FormPerfilProps) => {
-    const [isLoading, setIsLoading] = useState(false);
-
-    const [router, setRouter] = useState(useRouter());
-
-    const {
-        register,
-        handleSubmit,
-        setValue,
-        watch,
-        formState: {
-            errors,
-        }
-    } = useForm({
-        defaultValues: {
-            name: currentUser?.name,
-            image: currentUser?.image
-        }
-    });
-
-    const image = watch('image');
-
-    const handleUpload = (result: any) => {
-        setValue('image', result?.info?.secure_url, {
-            shouldValidate: true
-        })
-    }
-
-    /*const age = differenceInYears(new Date(), new Date(currentUser.bornDate)); */ 
-
+const FormPerfil: React.FC<FormPerfilProps> = (
+    {currentUser}
+) => {
+   
     return (
         <div className={styles.perfilForm}>
             <div className={styles.nombreGroup}>
                 <label id="nombre">{currentUser.name}</label>
             </div>
             <div className={styles.imgGroup}>
-                <img src={currentUser.image ?? ""} alt="icon" className={styles.imgPerfil}/> {/* Asegúrate de que 'image' es un campo en tu objeto User */}
+                <img src="/img/adult.jpg" alt="icon" className={styles.imgPerfil}/> {/* Asegúrate de que 'image' es un campo en tu objeto User */}
             </div>
             <div className={styles.edadGroup}>
                 <label>Edad: {" "}</label>
@@ -54,3 +27,6 @@ export const FormPerfil = ({ currentUser }: FormPerfilProps) => {
         </div>
     )
 }
+
+
+export default FormPerfil;
--- src/components/user/descripcion/FormPerfil/index.ts
+++
@@ -1,1 +0,0 @@
-import { FormPerfil } from './FormPerfil'; 
