d928ff75b44c95be9cccc11c4d7db25b4478acf8
solofervqz <solofervazquez@gmail.com>
2024-05-20 23:15:17 -0600

    ADD: UserCard into Dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,13 +4,19 @@ import { SignOutButton } from "@/components/shared/SignOutButton";
 import styles from "./dashboard.module.css";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import Navbar from "@/components/user/perfil/navbar/Navbar";
+import UsersList from '@/components/dashboard/UsersList';
+import getUsers from "../actions/getUsers";
 
 async function DashboardPage() {
   const currentUser = await getCurrentUser();
+  const users = await getUsers();
 
   return (
     <div className={styles.bgPrattle}>
       <Navbar currentUser={currentUser!} /> {/* Puedes pasar null o undefined mientras se carga */}
+      <div>
+        <UsersList title="Nuevos usuarios" items={users}/>
+      </div>
       <h1>Felicidades</h1>
       <p>Estás dentro uwu</p>
       <SignOutButton />
---
+++ src/components/dashboard/UsersList.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { isEmpty } from "lodash";
+import UserCard from "./UserCard";
+
+interface UsersListProps {
+    items: string[];
+    title: string;
+}   
+
+const UsersList = ({items, title}) => {
+    if(isEmpty(items)) {
+        return null;
+    }
+    
+    return ( 
+    <div className="px-4 md:px-12 mt-4 space-y-8">
+        <div>
+            <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
+                {title}
+            </p>
+            <div className="overflow-x-auto">
+                    <div className="flex space-x-4 mb-2">
+                        {items.map((item) => (
+                            <div key={item.id} className="flex-none w-1/4">
+                                <UserCard data={item} />
+                            </div>
+                        ))}
+                    </div>
+            </div>
+        </div>
+    </div> 
+    );
+}
+ 
+export default UsersList;
