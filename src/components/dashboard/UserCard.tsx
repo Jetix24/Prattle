25e0f6bea124120717c24fe60437d387ef0d8ede
solofervqz <solofervazquez@gmail.com>
2024-05-21 20:05:48 -0600

    Add: Title in User Card

--- src/components/dashboard/UserCard.tsx
+++ src/components/dashboard/UserCard.tsx
@@ -69,7 +69,7 @@ const UserCard: React.FC<UserCardProps> = ({
           )} 
         </div>
             <h5 className="mb-1 text-xl font-medium text-gray-900 dark:text-white">{data.name}</h5>
-            <span className="text-sm text-gray-500 dark:text-gray-400">Aqui va el titulo</span>
+            <span className="text-sm text-gray-500 dark:text-gray-400">{data?.title || "Estoy usando Prattle"}</span>
             <div className="flex mt-4 md:mt-6">
                 <button onClick={handleClick} className="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Mensajear</button>
                 <a href="#" className="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Ver perfil</a>
