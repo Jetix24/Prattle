3baad89878729a46f8b5416fe678b748bf124ee9
solofervqz <solofervazquez@gmail.com>
2024-05-22 23:08:33 -0600

    Change scrollbar color in UsersList

--- src/components/dashboard/UsersList.tsx
+++ src/components/dashboard/UsersList.tsx
@@ -19,7 +19,7 @@ const UsersList = ({items, title}) => {
                 <p className="text-white text-md md:text-xl lg:text-2xl font-semibold mb-4">
                     {title}
                 </p>
-                <div className="overflow-x-auto scrollbar scrollbar-track-slate-600 scrollbar-thumb-cyan-400">
+                <div className="overflow-x-auto scrollbar scrollbar-track-slate-700 scrollbar-thumb-slate-600">
                         <div className="flex space-x-4 mb-2">
                             {items.map((item) => (
                                 <div key={item.id} className="flex-none lg:w-1/4 w-1/1 md:w-1/3">
