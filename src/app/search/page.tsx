d6b6cc0abede273b89ac3e1ac321089c15d052a8
solofervqz <solofervazquez@gmail.com>
2024-05-23 01:47:50 -0600

    Fix: search api route 2.0

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,19 +1,30 @@
 "use client";
-import { useSearchParams } from "next/navigation";
-import DashboardPage from "../dashboard/page";
+import { Suspense } from 'react';
+import { useSearchParams } from 'next/navigation';
+import DashboardPage from '../dashboard/page';
+import Navbar from '@/components/user/perfil/navbar/Navbar';
 
 const SearchPage = () => {
-    const search = useSearchParams();
-    const searchQuery = search ? search?.get("q") : null;
+  const search = useSearchParams();
+  const searchQuery = search ? search?.get('q') : null;
 
-    const encodedSearchQuery = encodeURI(searchQuery || "");
+  const encodedSearchQuery = encodeURI(searchQuery || '');
 
-    console.log("Search query: ", searchQuery);
-    return ( 
-            <div>
-                <h1>Search Page</h1>
-            </div>
-     );
-}
- 
-export default SearchPage;
+  console.log('Search query: ', searchQuery);
+  return (
+    <>
+    
+    <div>
+      <h1>Search Page</h1>
+    </div>
+    </>
+  );
+};
+
+const SuspenseWrapper = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <SearchPage />
+  </Suspense>
+);
+
+export default SuspenseWrapper;
