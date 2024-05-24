e39ab4d2948c41bd6594772e83dfa345e4319948
solofervqz <solofervazquez@gmail.com>
2024-05-24 00:24:42 -0600

    Complete search api

--- pages/api/search.ts
+++ pages/api/search.ts
@@ -1,16 +1,40 @@
 import type { NextApiRequest, NextApiResponse } from "next";
+import prisma from "@/app/libs/prismadb";
 
 export default async function handler(
     req: NextApiRequest, 
     res: NextApiResponse) 
 {
-    if (req.method === 'GET') {
-        res.status(200).json({ name: "John Doe" });
-    } else if (req.method === 'POST') {
-        // Handle POST request
-        // You might want to use req.body to access the data sent in the POST request
-    } else {
-        // Handle other HTTP methods or return an error message
-        res.status(405).json({ message: 'Method Not Allowed' });
+    if (req.method === 'GET'){
+        try{
+            const { q:query } = req.query;
+
+            if(typeof query !== 'string'){
+                throw new Error('Invalid query');
+            }
+
+            const users = await prisma.user.findMany({
+                where: {
+                    OR: [
+                        {
+                            name: {
+                                contains: query,
+                                mode: 'insensitive'
+                            }
+                        },
+                        {
+                            email: {
+                                contains: query,
+                                mode: 'insensitive'
+                            }
+                        }
+                    ]
+                }
+            });
+
+            res.status(200).json({users}); // Aqui regresa 
+        } catch (error) {
+            res.status(500).end();
+        }
     }
 }
--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -3,17 +3,41 @@ import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 import DashboardPage from '../dashboard/page';
 import Navbar from '@/components/user/perfil/navbar/Navbar';
+import SearchBar from '@/components/user/perfil/navbar/SearchBar';
+import styles from '@/app/dashboard/dashboard.module.css';
+import useSWR from 'swr';
+
+const fetchPosts = async (url: string) => {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+        throw new Error('Failed to fetch posts');
+    }
+
+    return response.json();
+}
 
 const SearchPage = () => {
   const search = useSearchParams();
   const searchQuery = search ? search?.get('q') : null;
-
   const encodedSearchQuery = encodeURI(searchQuery || '');
+  
+  const { data, isLoading } = useSWR(`/api/search?q=${encodedSearchQuery}`, fetchPosts);
 
-  console.log('Search query: ', searchQuery);
+  
+
+  console.log('La data es:', data);
+
   return (
     <>
-    
+    <div className={styles.bgPrattle}>
+    <nav className="flex w-full items-center p-4">
+        <div className="my-2 mx-3 hidden lg:block">
+            <img src="/img/logo_blanc.png" className="h-24 w-auto object-contain" alt="Logo" />
+        </div>
+        <SearchBar />
+    </nav>
+    </div>
     <div>
       <h1>Search Page</h1>
     </div>
