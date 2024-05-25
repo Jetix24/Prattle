7b9bbd05a341c74d3d7cba0818c2e0e993f4f29d
Jetix24 <josepabloaguirre02@gmail.com>
2024-05-24 21:44:47 -0600

    Update: Intereses CSS 2.0

--- src/components/admin/intereses/FormIntereses/FormIntereses.tsx
+++ src/components/admin/intereses/FormIntereses/FormIntereses.tsx
@@ -9,6 +9,7 @@ import { toast } from "react-hot-toast";
 import { Categories } from '@prisma/client';
 import Select from '@/components/users/inputs/Select';
 import Input from "@/components/users/inputs/Input";
+import { MdNewLabel } from "react-icons/md";
 
 interface FormInteresesProps {
   categories: Categories[];
@@ -65,17 +66,29 @@ const FormIntereses: React.FC<FormInteresesProps> = ({ categories }) => {
   };
 
   return (
-    <section>
-      <div className={styles.container}>
+    <section className={styles.container}>
         <div className={styles.logoContainer}>
-          <h2>Registro de Intereses</h2>
+          <h2>Intereses</h2>
         </div>
-        <form onSubmit={handleSubmit(onSubmit)} className={styles.interesesForm}>
+        <form onSubmit={handleSubmit(onSubmit)} className={styles.interesesForm}>        
+          <label htmlFor="input-group-1" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Nombre</label>
+              <div className="relative">
+                <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+                <MdNewLabel className="w-6 h-6 text-icon-color dark:text-icon-color"/>      
+                </div>
+                <input 
+                  type="text" 
+                  id="input-group-1" 
+                  className="bg-input-gray text-gray-900 text-sm rounded hover:bg-white focus:outline-none focus:bg-white focus:ring-0 block w-full ps-10 p-2.5 transition-colors duration-200" 
+                  placeholder="Nombre del interes"
+                  disabled={isLoading} 
+                  {...register('name')}
+                  required 
+                />
+              </div>
+                
           <div className={styles.formGroup}>
-            <div className={styles.inputIconContainer}>
-              <Input label="Nombre" type="text" id="name" disabled={isLoading} register={register} required errors={errors} />
-              <img src="/icon/nombre.png" alt="icon" className={styles.inputIcon} />
-            </div>
+            
             <Select
               disabled={isLoading}
               label="Categorías"
@@ -96,7 +109,7 @@ const FormIntereses: React.FC<FormInteresesProps> = ({ categories }) => {
                 width="400"
                 height="400"
                 className="rounded-[10px]"
-                src={cover || "/img/placeholder.jpg"}
+                src={cover || "/img/agregar.png"}
                 alt="Avatar"
               />
             </div>
@@ -112,7 +125,6 @@ const FormIntereses: React.FC<FormInteresesProps> = ({ categories }) => {
             {isLoading ? 'Cargando...' : 'Guardar'}
           </button>
         </form>
-      </div>
     </section>
   );
 };
