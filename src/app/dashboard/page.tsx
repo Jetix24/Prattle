2b2f82f36bc7e5ff2f4d941b3b2308ee1be446ae
solofervqz <solofervazquez@gmail.com>
2024-05-26 01:09:08 -0600

    ADD: UserLists without interests in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -5,20 +5,24 @@ import Navbar from "@/components/user/perfil/navbar/Navbar";
 import UsersList from '@/components/dashboard/UsersList';
 import getUsers from "../actions/getUsers";
 import getUsersByInterest from '../actions/getUsersByInterest';
+import getUsersWithoutInterests from '../actions/getUsersWithoutInterests';
 
 async function DashboardPage() {
   const currentUser = await getCurrentUser();
   const users = await getUsers();
+  const first10Users = users.slice(0, 10); // Obtener los primeros 10 usuarios
   const interestsWithUsers = await getUsersByInterest();
+  const usersWithoutInterests = await getUsersWithoutInterests();
 
   return (
       <div className={`h-full ${styles.bgPrattle}`}>
         <Navbar currentUser={currentUser!} />
         <div>
-          <UsersList title="Nuevos usuarios" items={users}/>
+          <UsersList title="Nuevos usuarios" items={first10Users}/>
           {interestsWithUsers.map((interest) => (
             <UsersList key={interest.id} title={interest.name} items={interest.users} />
           ))}
+         <UsersList title="Usuarios sin intereses" items={usersWithoutInterests} /> 
         </div>
       </div>
   );
