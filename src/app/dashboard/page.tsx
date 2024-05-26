e6d35e3ad5de58e860ecabcd0cbaface63454963
solofervqz <solofervazquez@gmail.com>
2024-05-26 00:09:59 -0600

    ADD: UserLists with interests in dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,24 @@
 import { useEffect, useState } from 'react';
-import { NextPageContext } from "next";
 import styles from "./dashboard.module.css";
 import getCurrentUser from "@/app/actions/getCurrentUser";
 import Navbar from "@/components/user/perfil/navbar/Navbar";
 import UsersList from '@/components/dashboard/UsersList';
 import getUsers from "../actions/getUsers";
+import getUsersByInterest from '../actions/getUsersByInterest';
 
 async function DashboardPage() {
   const currentUser = await getCurrentUser();
   const users = await getUsers();
+  const interestsWithUsers = await getUsersByInterest();
 
   return (
-      <div className={`h-screen ${styles.bgPrattle}`}>
+      <div className={`h-full ${styles.bgPrattle}`}>
         <Navbar currentUser={currentUser!} />
         <div>
           <UsersList title="Nuevos usuarios" items={users}/>
+          {interestsWithUsers.map((interest) => (
+            <UsersList key={interest.id} title={interest.name} items={interest.users} />
+          ))}
         </div>
       </div>
   );
