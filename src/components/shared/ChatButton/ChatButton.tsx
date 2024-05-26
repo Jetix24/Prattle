7417aaeaf76cba6a615fb067c28c68915a1ef61b
solofervqz <solofervazquez@gmail.com>
2024-05-25 22:52:20 -0600

    Change url from users to friends

--- src/components/shared/ChatButton/ChatButton.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-import Link from "next/link";
-import styles from "./ChatButton.module.css"
-
-export const ChatButton = () => {
-    return (
-        <Link className={styles.ChatButton} href="/users">Chat</Link>
-    )
-}
